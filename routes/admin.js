a86a2c11baf3296c84a443e094ed3911c5d75346
abraham-1 <esp19005@byui.edu>
2021-09-30 20:29:24 -0500

    ejs pug and handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -6,17 +6,20 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
-router.get('/add-product',(req, res, next) => {
-    //res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>'); //The default response header is text html
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get('/add-product',(req, res, next) => {    
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product', activeAddProduct: true, productCSS: true});
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) =>{
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
 
