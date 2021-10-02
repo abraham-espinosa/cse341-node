11175bc4a67a80de8b8a847c2b947b7842718a74
abraham-1 <esp19005@byui.edu>
2021-10-02 11:30:32 -0500

    convert it to ejs

--- app.js
+++ app.js
@@ -2,29 +2,23 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-
-// app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'handlebars'})); 
-// app.set('view engine', 'handlebars'); 
-
 app.set('view engine', 'ejs'); 
-// app.set('view engine', 'pug'); 
 app.set('views', 'views');
 
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
-app.listen(3000); //const server = http.createServer(app); //server.listen(3000);
+app.listen(3000); 
